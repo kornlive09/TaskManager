57cd529c7dda0525a56a5243822797711042cb05
anton <anton>
2016-12-07 15:24:07 +0300

    Create

---
+++ Windows/Windows/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows
+{
+    class MainForm : Form1
+    {
+        public MainForm() : base()
+        {
+            this.Load += (sender, args) =>
+            {
+                
+            };
+        }
+    }
+}
